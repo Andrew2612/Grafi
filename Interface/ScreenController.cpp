392e962b6989371126bdea300f93e38cc5c4d94a
Andrew3840 <andrew.petuhov03@yandex.ru>
2023-11-26 15:24:57 +0300

    Buttons Add

--- Interface/ScreenController.cpp
+++ Interface/ScreenController.cpp
@@ -14,63 +14,20 @@ public:
     : window(sf::RenderWindow{sf::VideoMode(WIDTH, HEIGHT), "Grafi"}) {LoadMenu();}
     ~Screen() {DeleteButtons();}
 
+    Screen(const Screen& s) = delete;
+    Screen& operator=(const Screen& s) = delete;
+    Screen(Screen&& s) = delete;
+    Screen& operator=(Screen&& s) = delete;
+
     bool Open() const {return window.isOpen();}
 
-    void LoadScene1()
-    {
-        std::cerr << "Call\n";
-    }
+    void Close();
 
-    void LoadMenu()
-    {
-        DeleteButtons();
+    void LoadMenu();
 
-        sf::RectangleShape* bb = new sf::RectangleShape(sf::Vector2f(100, 100));
-        bb->setFillColor(sf::Color(255, 255, 0));
-        bb->setOrigin(sf::Vector2f(50, 50));
-        bb->setPosition(sf::Vector2f(WIDTH/2, HEIGHT/2));
+    void LoadScene1();
 
-
-        buttons.push_back(new Button(bb, this, &LoadScene1));
-    }
-
-    void Update()
-    {
-		while (window.pollEvent(event))
-		{
-			if (event.type == sf::Event::Closed)
-            {
-                window.close();
-            }
-
-            if (event.type == sf::Event::MouseButtonPressed
-                && event.mouseButton.button == sf::Mouse::Left)
-            {
-                mouse_pos = sf::Mouse::getPosition(window);
-                std::cerr << (*buttons[0]->Shape()).getGlobalBounds().left << '\n';
-                std::cerr << (*buttons[0]->Shape()).getGlobalBounds().top << '\n';
-                std::cerr << (*buttons[0]->Shape()).getGlobalBounds().width << '\n';
-                std::cerr << (*buttons[0]->Shape()).getGlobalBounds().height << '\n';
-                std::cerr << mouse_pos.x << '\n';
-                std::cerr << mouse_pos.y << '\n';
-                for (u32 i = 0; i < buttons.size(); i++)
-                {
-                    if ((*buttons[i]->Shape()).getGlobalBounds().contains(mouse_pos.x, mouse_pos.y))
-                    {
-                        buttons[i]->Call();
-                    }
-                }
-            }
-		}
-        window.clear(sf::Color::White);
-
-        for (u32 i = 0; i < buttons.size(); i++)
-        {
-            window.draw(*buttons[i]->Shape());
-        }
-
-        window.display();
-    }
+    void Update();
 
 private:
     void DeleteButtons()
@@ -93,3 +50,70 @@ private:
     std::vector<Button*> buttons;
 };
 
+void Screen::Close()
+{
+    DeleteButtons();
+    window.close();
+}
+
+void Screen::LoadMenu()
+{
+    DeleteButtons();
+
+    sf::RectangleShape* bb = new sf::RectangleShape(sf::Vector2f(100, 100));
+    bb->setFillColor(sf::Color(255, 255, 0));
+    bb->setOrigin(sf::Vector2f(50, 50));
+    bb->setPosition(sf::Vector2f(WIDTH/2, HEIGHT/2 - 60));
+
+    sf::RectangleShape* bb1 = new sf::RectangleShape(sf::Vector2f(100, 100));
+    bb1->setFillColor(sf::Color(255, 255, 0));
+    bb1->setOrigin(sf::Vector2f(50, 50));
+    bb1->setPosition(sf::Vector2f(WIDTH/2, HEIGHT/2 + 60));
+
+    buttons.push_back(new Button(bb, this, &Close));
+    buttons.push_back(new Button(bb1, this, &LoadScene1));
+}
+
+void Screen::LoadScene1()
+{
+    DeleteButtons();
+
+    sf::RectangleShape* bb = new sf::RectangleShape(sf::Vector2f(100, 100));
+    bb->setFillColor(sf::Color(255, 255, 100));
+    bb->setOrigin(sf::Vector2f(50, 50));
+    bb->setPosition(sf::Vector2f(WIDTH/2, HEIGHT/2));
+
+    buttons.push_back(new Button(bb, this, &Close));    
+}
+
+void Screen::Update()
+{
+    while (window.pollEvent(event))
+    {
+        if (event.type == sf::Event::Closed)
+        {
+            Close();
+        }
+
+        if (event.type == sf::Event::MouseButtonPressed
+            && event.mouseButton.button == sf::Mouse::Left)
+        {
+            mouse_pos = sf::Mouse::getPosition(window);
+            for (u32 i = 0; i < buttons.size(); i++)
+            {
+                if ((*buttons[i]->Shape()).getGlobalBounds().contains(mouse_pos.x, mouse_pos.y))
+                {
+                    buttons[i]->Call();
+                }
+            }
+        }
+    }
+    window.clear(sf::Color::White);
+
+    for (u32 i = 0; i < buttons.size(); i++)
+    {
+        window.draw(*buttons[i]->Shape());
+    }
+
+    window.display();
+}
